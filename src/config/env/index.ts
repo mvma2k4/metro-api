35557658581c5f6c5cb168a4a8be41bfb008a13b
Marcel Morales <mvma2k4@yahoo.com>
2020-01-02 21:57:58 -0400

    Update new database url

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -25,8 +25,8 @@ const development: IConfig = {
 const production: IConfig = {
     port: process.env.PORT || 3000,
     database: {
-        DB_URI: process.env.DB_URI || 'postgres://oyduwnotbnmomt:e60bd887112eb3c6d61d9c41431fe0d4241ce36f970f1526677a7f33a5727778@ec2-54-235-167-210.compute-1.amazonaws.com:5432/',
-        DB_DB_MAIN: process.env.DB_DB_MAIN || 'dbkprlm0rb1sse'
+        DB_URI: process.env.DB_URI || 'postgres://vkpzisciajmczk:19415d4ea997787fc46ad002cddb3a3760583a0dd77bb217eed4852c24e235b4@ec2-174-129-33-13.compute-1.amazonaws.com:5432/',
+        DB_DB_MAIN: process.env.DB_DB_MAIN || 'db0v33valq2f96'
     },
     secret: process.env.SECRET || '@QEGTUI'
 };
